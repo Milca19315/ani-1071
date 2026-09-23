3b8ed8187d96bba8ca8883fb5d85b0fe5f8a3358
Milca19315 <milcaguylene@gmail.com>
2026-09-23 20:23:44 +0200

    Add remarks on compilation and executable behavior
    Added remarks about compiling and executable naming.

---
+++ chapitre-01/exo5-deux_fichiers_deux_noms/c1-exo5_reponse.cpp
@@ -0,0 +1,6 @@
+Remarque:
+    Après la compilation du fichier source c1-exo5_main.cpp deux fois en essai_un.exe et essai_deux.exe, on constate que les deux éxécutables affichent le meme message (Bonjour).
+
+    Par ailleurs, le nom du fichier source (.cpp) et le nom du programme n'ont aucun lien.
+
+    On peut donc conclure que l'option -o reli le fichier source au programme. Du coup, on peut créer plusieurs programmes différents à partir du meme fichier d'extension .cpp.

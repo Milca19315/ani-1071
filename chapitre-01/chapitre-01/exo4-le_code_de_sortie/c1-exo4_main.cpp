7282039ae843d60d1fe681c5e1ce522cefa9c892
Milca19315 <milcaguylene@gmail.com>
2026-09-23 04:29:56 +0200

    Create c1-exo4_main.cpp with exit code example
    Add a main function that prints 'Bonjour' and returns 3.

---
+++ chapitre-01/chapitre-01/exo4-le_code_de_sortie/c1-exo4_main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+    int main(){
+
+            std::cout<<"Bonjour"<< std::endl;
+    return 3;
+    }
+    // Le code de sortie correspond à la valeur obtenue après compilation du programme (dans notre cas, 3). Ce nombre pourrait permettre aux outils qui enchainent les programmes, 
+    // de vérifier le code à chaque fois, pour donner la décision de s'arreter ou de continuer le programme.
+    

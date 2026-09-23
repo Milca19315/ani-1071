5d69307bb0340e82122a311b945b4e8b474b2cc2
Milca19315 <milcaguylene@gmail.com>
2026-09-23 02:26:25 +0200

    Add main.cpp to display strings on the same line
    This program displays two strings without a newline between them.

---
+++ chapitre-01/chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+    int main(){
+
+            std::cout<<"BEHEMI GUYLENE MILCA";
+            std::cout<<"Yaoundé";
+    }
+    // Remarque : Ce programme affiche deux chaînes de caractères sans passer à la ligne entre elles.
+    // Sans l'utilisation de std::endl, mes deux messages s'affivheront sur la même ligne.
